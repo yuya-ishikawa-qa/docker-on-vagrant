8f6229b9bbe31336798a3e763c4bba3885f5ae16
Yuya Ishikawa <yuya.quest.academia@gmail.com>
2020-06-07 16:19:04 +0900

    create Vagrantfile

---
+++ docker-vagrant_init.sh
@@ -0,0 +1,13 @@
+sudo yum install -y yum-utils
+sudo yum install -y device-mapper-persistent-data
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+docker -v
+
+mkdir -p /home/vagrant/.local/bin/
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /home/vagrant/.local/bin/docker-compose
+sudo chmod 775 /home/vagrant/.local/bin/docker-compose
+docker-compose -v
